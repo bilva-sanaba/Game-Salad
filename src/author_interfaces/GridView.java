8317d20c29d65d3c6097d0554612b728a283de01
Jack Bloomfeld <jfb25@duke.edu>
2017-04-02 17:45:36 -0400

    Added some classes

--- src/author_interfaces/GridView.java
+++ src/author_interfaces/GridView.java
@@ -1,9 +1,10 @@
 package author_interfaces;
 
+import author_interfaces.GUIComponent;
 import gameEngine_interface.Sprite;
 import javafx.scene.layout.GridPane;
 
-public interface GridView {
+public interface GridView extends GUIComponent {
 	public void placeSprite(Sprite s);
 	public GridPane createGridView(GridViewData gvd);
 	public GridPane getGridPane();
---
+++ src/author_interfaces/Sprite.java
@@ -0,0 +1,5 @@
+package author_interfaces;
+
+public class Sprite {
+
+}
