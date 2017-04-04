9a19c0bb68472677ae7cad61009a6649b634470d
Joshua Kopen <jmk86@duke.edu>
2017-04-04 17:33:53 -0400

    Created a basic ImageSaver file

--- src/data_interfaces/ImageSaver.java
+++ src/data_interfaces/ImageSaver.java
@@ -1,11 +1,29 @@
 package data_interfaces;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import javafx.scene.image.Image;
+
 public class ImageSaver implements FileSaver{
 
 	
-	public void createFile(String fileName, String data) {
+	public void createFile(String fileName, Object data) {
 		
-		
+		try {
+			BufferedImage bi = (BufferedImage)data;
+			File f = new File(fileName);
+			ImageIO.write(bi, "jpg", f);
+		}
+		catch (IOException e) {
+			//TODO use their error alert
+		}
 	}
 
+	public void saveImage(String fileName, Image i) {
+		createFile(fileName, i);
+	}
 }
