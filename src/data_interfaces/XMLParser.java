82eb89f85c257fdfbd8ac47cb8e7c1eb13152e52
Joshua Kopen <jmk86@duke.edu>
2017-04-04 23:12:05 -0400

    Added the ability to read a file

--- src/data_interfaces/XMLParser.java
+++ src/data_interfaces/XMLParser.java
@@ -1,12 +1,40 @@
 package data_interfaces;
 
+import java.util.List;
+import java.io.*;
+
 public class XMLParser implements FileLoader {
 
 	@Override
-	public String loadFile(String fileName) {
-		// TODO Auto-generated method stub
+	public Object loadFile(String fileName) {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			StringBuilder sb = new StringBuilder();
+			String temp = reader.readLine();
+			while (temp != null) {
+				sb.append(temp);
+				temp = reader.readLine();
+			}
+			return sb.toString();
+		}
+		catch (IOException e) {
+			//TODO 
+		}
 		return null;
 	}
-
 	
+	/**
+	 * Retrieves the data needed from the file
+	 * to be sent to the game engine
+	 * @param fileName the name of the file
+	 * @return the list of reflected objects
+	 */
+	/*public List getData(String fileName) {
+		
+	}*/
+
+	public static void main(String [] args) {
+		XMLParser x = new XMLParser();
+		System.out.println(x.loadFile("games/rub.txt"));
+	}
 }
