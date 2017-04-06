1fa562b814801bcf40c916b6278a725b973781ec
Jacob Weiss <jacob.weiss@duke.edu>
2017-04-06 02:02:44 -0400

    Included observable and animation in controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,26 +3,38 @@ package controller;
 import gameView.UIImageModel;
 import gameView.UIImageProperty;
 import gameView.UIView;
+import gameView.WorldAnimator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Observable;
+import java.util.Observer;
 
 import view_interfaces.UIViewInterface;
 import javafx.stage.Stage;
 import controller_interfaces.ControllerInterface;
 import gameEngine_interface.GameEngine;
 
+/**
+ * 
+ * @author Jacob
+ *
+ */
 
-
-public class Controller implements ControllerInterface {
+public class Controller implements ControllerInterface, Observer {
 	
 	UIViewInterface myGameView;
 	private GameEngine myGameEngine;
+	private Observable myObservable;
+	private WorldAnimator myWorldAnimator;
+	private Stage myStage;
 	
 	public Controller(Stage s) {
+		myStage = s;
 		myGameView = new UIView(s, this);
 		myGameEngine = new GameEngine();
+		myWorldAnimator = new WorldAnimator();
 	}
 	
 	public Controller() {
@@ -48,6 +60,7 @@ public class Controller implements ControllerInterface {
 	@Override
 	public void loadNewGame(String filePath) {
 		// TODO Auto-generated method stub
+		RestrictedEntityManager restrictedEntityManager = myGameEngine.getRestrictedEntityManager();
 		
 	}
 
@@ -61,6 +74,20 @@ public class Controller implements ControllerInterface {
 	@Override
 	public void checkCollision(UIImageModel u) {
 		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Observable obs, Object arg) {
+		// TODO Auto-generated method stub
+		//call world animator
+		Collection<RestrictedEntity> animatableEntities = (Collection<RestrictedEntity>) arg;
+		myWorldAnimator.start(myStage, animatableEntities);
+	}
+	
+	
+	
+	private void handleKeyInput(KeyCode code){
 		
 	}
 	
--- src/gameView/WorldAnimator.java
+++ src/gameView/WorldAnimator.java
@@ -12,41 +12,31 @@ import javafx.util.Duration;
  *
  */
 public class WorldAnimator {
-	public final String TITLE = "Example Game";
+	private Stage myStage;
 	public static final int FRAMES_PER_SECOND = 60;
 	public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
 	public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
 	public static final int KEY_INPUT_SPEED = 3;
+	
 	private EntityFactory entityFactory = new EntityFactory();
 	
-	public void start (Stage s){
+	public WorldAnimator(){
+	}
+	
+	public void start (Stage s, Collection<RestrictedEntity> restrictedEntities){
 		
 		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY),
-									  e-> step(SECOND_DELAY));
+									  e-> step(SECOND_DELAY, restrictedEntities));
 		Timeline animation = new Timeline();
 		animation.setCycleCount(Timeline.INDEFINITE);
 		animation.getKeyFrames().add(frame);
 		animation.play();
 	}
 	
-	private void step(double elapsedTime){
-		//loop through list of entities and make appropriate update to their position by using Factory/reflection
+	private void step(double elapsedTime, Collection<RestrictedEntities> restrictedEntities){
+		for(Entity e : restrictedEntities){
 			Entity updatedEntity = entityFactory.genEntity(DataType);
 			updatedEntity.move();
-	}
-	
-	private void handleKeyInput(KeyCode code){
-		if(code==KeyCode.RIGHT){
-			
-		}
-		else if(code==KeyCode.LEFT){
-			
-		}
-		else if(code==KeyCode.UP){
-			
-		}
-		else if(code==KeyCode.DOWN){
-			
 		}
 	}
 }
