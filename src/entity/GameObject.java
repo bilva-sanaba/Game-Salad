6ded318ad6e67eb66a63970138eac25db5d92bed
Jack Bloomfeld <jfb25@duke.edu>
2017-04-07 20:24:51 -0400

    Made GameObject and Level classes

---
+++ src/entity/GameObject.java
@@ -0,0 +1,12 @@
+package entity;
+
+import java.util.ArrayList;
+
+public class GameObject {
+	private ArrayList<Level> levelList = new ArrayList<Level>();
+
+	public ArrayList<Level> getLevels(){
+		return levelList;
+	}
+	
+}
