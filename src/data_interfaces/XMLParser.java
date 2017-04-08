2f05cf738ee68884e75ac8f44b8120220da7e991
Joshua Kopen <jmk86@duke.edu>
2017-04-08 18:11:20 -0400

    Changed these to utilize a dom parser for easier creation of objects

--- src/data_interfaces/XMLParser.java
+++ src/data_interfaces/XMLParser.java
@@ -2,6 +2,18 @@ package data_interfaces;
 
 
 import java.util.List;
+import voogasalad.util.reflection.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+
+
 import java.io.*;
 
 
@@ -11,21 +23,22 @@ import entity.IEntityManager;
 
 public class XMLParser implements FileLoader {
 
-	@Override
+	
 
 	public Object loadFile(String fileName) {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(fileName));
-			StringBuilder sb = new StringBuilder();
-			String temp = reader.readLine();
-			while (temp != null) {
-				sb.append(temp);
-				temp = reader.readLine();
-			}
-			return sb.toString();
-		}
-		catch (IOException e) {
-			//TODO 
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse(fileName);
+			return doc.getDocumentElement();
+		} catch (ParserConfigurationException e) {
+			// TODO Solve this	
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return null;
 	}
@@ -36,12 +49,12 @@ public class XMLParser implements FileLoader {
 	 * @param fileName the name of the file
 	 * @return the list of reflected objects
 	 */
-	/*public List getData(String fileName) {
-		
-	}*/
-
+	public Document getData(String fileName) {
+		return (Document)loadFile(fileName);
+	}
+	/*
 	public static void main(String [] args) {
 		XMLParser x = new XMLParser();
-		System.out.println(x.loadFile("games/rub.txt"));
-	}
+		System.out.println(x.getData("games/try2.xml"));
+	}*/
 }
