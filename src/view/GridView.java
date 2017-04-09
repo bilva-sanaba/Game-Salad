842869da09e77e7b8e1b5233d723452212903eb5
Jack Bloomfeld <jfb25@duke.edu>
2017-04-09 02:43:52 -0400

    Implemented placing entities on GridView

--- src/view/GridView.java
+++ src/view/GridView.java
@@ -1,7 +1,13 @@
 package view;
 
+import components.ComponentType;
+import components.LocationComponent;
+import components.SpriteComponent;
+import entity.Entity;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
@@ -15,20 +21,20 @@ import javafx.scene.paint.Color;
 public class GridView extends GUIComponent {
 	private GridPane myGrid;
 	private ViewData myData;
-	
+
 	public GridView(UtilityFactory utilF, ViewData data, int rows, int cols) {
 		myData = data;
 		myGrid = new GridPane();
 		myGrid.getStyleClass().add("view-grid");
 		myGrid.setAlignment(Pos.CENTER);
-		
+
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
 				addMouseListenerPane(row, col);
 			}
 		}
 	}
-	
+
 	private void addMouseListenerPane(int row, int col) {
 		Rectangle rect = new Rectangle(40, 40);
 		rect.getStyleClass().add("view-grid-cell");
@@ -37,23 +43,26 @@ public class GridView extends GUIComponent {
 			@Override
 			public void handle(MouseEvent event) {
 				System.out.println(String.format("Click at row %d col %d", row, col));
-				if (myData.getUserSelectedEntity() != null) {
-					
+				Entity userSelectedEntity = myData.getUserSelectedEntity();
+				if (userSelectedEntity != null) {
+					Entity placedEntity = userSelectedEntity.clone();
+					myData.placeEntity(placedEntity);
+					myData.setEntityLocation(placedEntity.getID(), row, col);
+					drawEntity(placedEntity);
 				}
 			}
 		});
 		myGrid.add(rect, row, col);
 	}
-	
-//	public void loadData(ViewData data) {
-//		myLevel = level;
-//		myLevel.addObserver(this);
-//	}
-//	
-//	public void addLevelEntity(Entity entity) {
-//		myLevel.add(entity);
-//	}
-	
+
+	private void drawEntity(Entity entity) {
+		LocationComponent entityLocation = (LocationComponent) entity.getComponent(ComponentType.Location);
+		SpriteComponent entitySprite = (SpriteComponent) entity.getComponent(ComponentType.Sprite);
+		Image a = entitySprite.getSprite();
+		ImageView spriteImage = new ImageView(entitySprite.getSprite());
+		myGrid.add(spriteImage, entityLocation.getX(), entityLocation.getY());
+	}
+
 	@Override
 	public Region buildComponent() {
 		return myGrid;
--- src/view/ViewData.java
+++ src/view/ViewData.java
@@ -4,39 +4,63 @@ import entity.Entity;
 
 import java.util.HashMap;
 import java.util.Observable;
+import components.*;
 
+/**
+ * Casting takes place to be able to edit the component because we are using enums to choose the specific component
+ * we feel comfortable to cast the component to its specific component
+ *
+ * @author Jonathan
+ * @author Justin
+ *
+ */
 public class ViewData extends Observable {
-	private HashMap<Integer, Entity> entityList;
+	private HashMap<Integer, Entity> definedEntityList;
+	private HashMap<Integer, Entity> placedEntityList;
 	private Entity userSelectedEntity;
-	
+
 	public ViewData() {
-		entityList = new HashMap<Integer, Entity>();
+		definedEntityList = new HashMap<Integer, Entity>();
+		placedEntityList = new HashMap<Integer, Entity>();
 		userSelectedEntity = null;
 	}
-	
+
 	public void setUserSelectedEntity(Entity entity) {
 		userSelectedEntity = entity;
 	}
-	
+
 	public Entity getUserSelectedEntity() {
 		return userSelectedEntity;
 	}
-	
+
 	public void setEntityLocation(int entityID, int row, int col) {
-//		entityList.get(entityID).getComponent(ComponentType.Location).setX(row);
+		LocationComponent locComp = (LocationComponent) definedEntityList.get(entityID).getComponent(ComponentType.Location);
+		locComp.setX(row);
+		locComp.setY(col);
 	}
-	
-	public void addEntity(Entity entity) {
-		entityList.put(entity.getID(), entity);
+
+	public void defineEntity(Entity entity) {
+		definedEntityList.put(entity.getID(), entity);
 		notifyObservers();
 	}
-	
-	public void removeEntity(Entity entity) {
-		entityList.remove(entity.getID());
+
+	public void placeEntity(Entity entity) {
+		placedEntityList.put(entity.getID(), entity);
 		notifyObservers();
 	}
-	
+
+	public void undefineEntity(Entity entity) {
+		definedEntityList.remove(entity.getID());
+		notifyObservers();
+	}
+
+	public void unplaceEntity(Entity entity) {
+		definedEntityList.remove(entity.getID());
+		notifyObservers();
+	}
+
 	public HashMap<Integer, Entity> getEntityMap() {
-		return entityList;
+		return definedEntityList;
+
 	}
 }
