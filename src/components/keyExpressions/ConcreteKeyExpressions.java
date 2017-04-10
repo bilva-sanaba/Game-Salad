4ef6898ad5a901d1e2bd0f5ab78b16b613ca0731
Bilva Sanaba <bgs21@duke.edu>
2017-04-10 01:49:18 -0400

    Added class with concrete key lamdas

--- src/components/keyExpressions/ConcreteKeyExpressions.java
+++ src/components/keyExpressions/ConcreteKeyExpressions.java
@@ -1,5 +1,27 @@
 package components.keyExpressions;
 
+import components.ComponentType;
+import components.KeyExpression;
+import components.VelocityComponent;
+import entity.IEntity;
+import javafx.scene.input.KeyCode;
+
 public enum ConcreteKeyExpressions {
+	JUMP ((a) -> {
+		VelocityComponent vc = (VelocityComponent) a.getComponent(ComponentType.Velocity);
+		if (vc.getY()==0){
+			vc.setY(50);
+		}
+	});
+
+    private KeyExpression keyExpression; 
+
+    ConcreteKeyExpressions(KeyExpression ke) {
+        this.keyExpression = ke;
+    }
+    
+    public KeyExpression getKeyExpression(){
+    	return keyExpression;
+    }
 
 }
