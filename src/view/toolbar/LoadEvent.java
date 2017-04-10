cc9c3785be8aef770707d1a96721f144b7f3057e
Joshua Kopen <jmk86@duke.edu>
2017-04-09 22:41:46 -0400

    Includes a correct classpath. The rest was partner programmed with Jack to make the load button functional in the authoring environment

--- src/view/toolbar/LoadEvent.java
+++ src/view/toolbar/LoadEvent.java
@@ -25,7 +25,6 @@ public class LoadEvent extends GameSavingDataTool implements ToolBarButtonEvent 
 	public void event() {
 		Communicator c;
 		Stage newStage = new Stage();
-		ViewData newVD = new ViewData();
 		FileChooser fc = new FileChooser();
 		fc.setTitle("Choose the file to load: ");
 		fc.setInitialDirectory(new File(System.getProperty("user.dir")));
@@ -40,19 +39,19 @@ public class LoadEvent extends GameSavingDataTool implements ToolBarButtonEvent 
 			String firstSplit = splitS[splitS.length - 1];
 			String name = firstSplit.substring(0, firstSplit.length()
 					- getSuffix().length());
-			newVD.setGameName(name);
+			myData.setGameName(name);
 			c = new Communicator(name);
 			Collection <Entity> col = c.getData();
+			myData.clearData();
 			for (Entity e: col) {
 				if (isPlaced(e)) {
 					System.out.println("this happens");
-					newVD.placeEntity(e);
+					myData.placeEntity(e);
 				}
 				else {
-					newVD.defineEntity(e);
+					myData.defineEntity(e);
 				}
 			}
-			myData = newVD;
 			myData.refresh();
 		}
 		
