d0ef80bfce8ebe9b66ca613f607ae6dd776c5fba
Belal Taher <bt72@duke.edu>
2017-04-11 11:27:32 -0400

    started game state manager

---
+++ src/controller/GameStateManager.java
@@ -0,0 +1,20 @@
+package controller;
+
+import components.HealthComponent;
+
+/*
+ * @author Belal Taher
+ */
+
+public class GameStateManager {
+	
+	private HealthComponent playerHealth;
+
+	public GameStateManager(){
+		
+	}
+	
+	public void endGame(){
+		
+	}
+}
