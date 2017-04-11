50dbf9132a129f5ec975854b5507529eee3d0ddd
Bilva Sanaba <bgs21@duke.edu>
2017-04-11 17:22:10 -0400

    where my stuff

--- src/gameEngine_interface/GameEngine.java
+++ src/gameEngine_interface/GameEngine.java
@@ -118,7 +118,7 @@ public class GameEngine implements GameEngineInterface {
 		((KeyInputComponent) t.getComponent(ComponentType.KeyInput)).addToMap(KeyCode.D,ConcreteKeyExpressions.RIGHT.getKeyExpression());
 		e.add(t);
 		myEntityManager=new EntityManager(e);    
-		myEngines = Arrays.asList(new NewMovementEngine(myEntityManager), new InputEngine(myEntityManager));
+		myEngines = Arrays.asList(new NewMovementEngine(myEntityManager), new InputEngine(myEntityManager), new CollisionEngine(myEntityManager));
 		initializeRestrictedEntities();
 	}
 }
