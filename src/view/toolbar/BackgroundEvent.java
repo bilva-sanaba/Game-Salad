f15a769189c96f97d9ed1501f1e906fb0c77ef32
jyang301 <jy183@duke.edu>
2017-04-11 20:17:24 -0400

    user can set background image

--- src/view/toolbar/BackgroundEvent.java
+++ src/view/toolbar/BackgroundEvent.java
@@ -1,5 +1,23 @@
 package view.toolbar;
 
-public class BackgroundEvent {
+import view.ImageChooser;
+import view.ViewData;
+
+public class BackgroundEvent implements ToolBarButtonEvent {
+	
+	private ViewData myData;
+	
+	public BackgroundEvent(ViewData myD) {
+		myData = myD;
+	}
+
+	@Override
+	public void event() {
+		ImageChooser ic = new ImageChooser();
+		String filePath = ic.chooseFile();
+		
+		myData.getLevelEntity().setBackgroundFilePath(filePath);
+		myData.refresh();
+	}
 
 }
