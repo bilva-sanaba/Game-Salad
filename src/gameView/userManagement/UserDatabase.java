30cfdf4939e81be5f88312ec476c389287d2f6e8
Henry Taylor <hjt8@duke.edu>
2017-04-25 12:29:59 -0400

    user reader/writer fixed and operating

--- src/gameView/userManagement/UserDatabase.java
+++ src/gameView/userManagement/UserDatabase.java
@@ -1,29 +1,20 @@
 package gameView.userManagement;
 
 import gameView.tools.FrontEndException;
-
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-
-import javax.xml.bind.annotation.XmlElement;
-
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
-import data_interfaces.XMLParser;
 
 public class UserDatabase {
 	
 	private final String DATA_FILE = "data" + File.separator + "UserData.xml"; 
 	
-	private Element myData;
 	private HashMap<String, UserData> myUsers;
+	private UserDataHandler myHandler;
 	
 	public UserDatabase() {
-		createUsers(new XMLParser().getData(DATA_FILE));
-		System.out.println(myUsers.values());
+		myHandler = new UserDataHandler();
+		myUsers = myHandler.loadUsers(DATA_FILE);
+		System.out.println(myUsers);
 	}
 	
 	
@@ -46,36 +37,8 @@ public class UserDatabase {
 	}
 	
 	public void saveUsers() {
-		Collection<UserData> collection = myUsers.values();
-		new UserDataWriter().saveUsers(DATA_FILE, myUsers);
-	}
-	
-	private void createUsers(Element data) {
-		myUsers = new HashMap<String, UserData>();
-		NodeList values = data.getElementsByTagName("user");
-		if (values != null) {
-			for (int i = 0; i < values.getLength(); i++) {
-				Element ele = (Element) values.item(i);
-				UserData newUser = getUserData(ele);
-				myUsers.put(newUser.getName(), newUser);
-			}
+		if (myUsers != null) {
+			myHandler.saveUsers(DATA_FILE, myUsers);
 		}
 	}
-	
-	private UserData getUserData(Element root) {
-		UserData newUser;
-		ArrayList<String> information = new ArrayList<String>();
-		UserData.DATA_FIELDS.stream()
-			.forEach(c -> {
-				NodeList values = root.getElementsByTagName((String) c);
-				if (values.getLength() != 1) {
-					throw new FrontEndException(String.format("ERROR IN USER XML FORMATTING WITH: %s", (String) c));
-				} else {
-					information.add(values.item(0).getTextContent());
-				}
-			});
-		newUser = new UserData(information.get(0), information.get(1), information.get(2));
-		return newUser;
-	}
-
 }
