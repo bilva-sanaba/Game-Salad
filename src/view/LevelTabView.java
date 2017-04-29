605ce91b3bdb8ad0151d606b51dba9da58f40943
Jack Bloomfeld <jfb25@duke.edu>
2017-04-29 02:09:56 -0400

    TabPane that supports levels in the authoring environment

--- src/view/LevelTabView.java
+++ src/view/LevelTabView.java
@@ -1,13 +1,26 @@
 package view;
 
+import java.util.ArrayList;
+
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.Region;
 
-public class LevelTabView {
+public class LevelTabView extends GUIComponent{
+	private ArrayList<LevelTab> tabsList;
 	private TabPane myTabs;
 	
-	public LevelTabView() {
+	public LevelTabView(GridView myGrid) {
 		myTabs = new TabPane();
+		LevelTab tab1 = new LevelTab(myGrid, 1);
+		tabsList = new ArrayList<LevelTab>();
+		tabsList.add(tab1);
+		myTabs.getTabs().add(tab1);
+		
+	}
+	
+	public ArrayList<LevelTab> getTabsList(){
+		return tabsList;
 	}
 	
 	public void loadTab(ScrollPane grid, int counter) {
@@ -16,5 +29,10 @@ public class LevelTabView {
 	
 	public void addNewTab(int counter) {
 		
+	}
+
+	@Override
+	public Region buildComponent() {
+		return myTabs;
 	}
 }
