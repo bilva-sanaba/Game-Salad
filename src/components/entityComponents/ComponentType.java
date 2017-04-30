029b0b3f224598a02cc3cc53422f879d89eb0bbf
Jonathan Rub <jonathan.rub@duke.edu>
2017-04-30 15:42:17 -0400

    fixed a comma tht broke everything in authoring

--- src/components/entityComponents/ComponentType.java
+++ src/components/entityComponents/ComponentType.java
@@ -42,7 +42,7 @@ public enum ComponentType {
 	InfiniteBlockAction(),
 	Controllable(),
 	Orientation(),
-	MonsterType();
+	MonsterType(),
 	Checkpoint(),
 	Null()
 	;
--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -72,6 +72,7 @@ public class Entity extends Observable implements IEntity, IRestrictedEntity {
 
 	@Override
 	public void addComponent(IComponent component) {
+		System.out.println(myComponentMap);
 		if (myComponentMap.containsKey(component)){
 				myComponentMap.remove(component);
 		}
