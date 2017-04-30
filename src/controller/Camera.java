52b302b5653d731647662f5480ca78f3c3fc3138
Hamsa <hsp8@duke.edu>
2017-04-30 16:22:02 -0400

    Trying to fix background

--- src/controller/Camera.java
+++ src/controller/Camera.java
@@ -13,6 +13,7 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
@@ -31,6 +32,7 @@ public class Camera extends Entity {
 	private LocationComponent myLC;
 	private double myFrameWidth;
 	private Rectangle background;
+	private double lcInitial;
 	
 	public Camera (int length, Scene myScene, LocationComponent lc , int id) {
 		super(id);
@@ -39,27 +41,56 @@ public class Camera extends Entity {
 		root = (Group) myFrame.getRoot();
 		myLC = lc;
 		myFrameWidth= myFrame.getWidth();
+		lcInitial = myLC.getX();
+		//root.setLayoutX(-myLC.getX() + myFrameWidth / 2);
+		
 
 		if (getComponent(ComponentType.Type) == null) {
 			addComponent(new TypeComponent(EntityType.Camera));
 		}
+		//root.prefWidth(100);
+
+		Rectangle r = new Rectangle();
+		r.setWidth(10000);
+		r.setHeight(10000);
+		r.setX(-500);
+		r.setY(-500);
+		r.setFill(Color.LIGHTGRAY);
+		//root.getChildren().add(r);
+//		root.setTranslateX(-myLC.getX() + myFrameWidth / 2 + 200
+//				);
+
 		background = new Rectangle();
 		background.setFill(new ImagePattern(new Image(getClass().getClassLoader().getResource("background1.png").toString())));
 		System.out.println(root.getLayoutX() + " is layout x");
+		background.setScaleX(1
+				);
+		
 
-		background.setLayoutX(-root.getTranslateX());
+		background.setLayoutX(root.getTranslateX() - myFrameWidth);
+		System.out.println(root.getTranslateX() + " " + myLC.getX() + " is the diff");
 		
 
 		background.setLayoutY(-root.getTranslateY() -200);
-		background.setWidth(myFrame.getWidth());
+		background.setWidth(myFrameWidth);
 		background.setHeight(myFrame.getHeight()/8);
+		String image = getClass().getClassLoader().getResource("background1.png").toExternalForm();
+		root.setStyle("-fx-background-image: url('" + image + "'); " +
+		           "-fx-background-position: center center; " +
+		           "-fx-background-repeat: stretch;");
+		
 		root.getChildren().add(background);
 		
 	}
 
 	public void updateCamera() {
-
+		
 		root.setTranslateX(-myLC.getX() + myFrameWidth / 2);
+		background.setX(myLC.getX());
+		
+		
+		//background.setWidth(myFrame.getWindow().getWidth());
+
 		
 		/*if(playerLocRelativeToCam <= LEFT_BOUND_FROM_EDGE && followerLoc.getX()>0){
 			root.setTranslateX(root.getTranslateX() - followerVel.getX() + 4);
