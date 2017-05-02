d0d0da9fcbc5c556dbc08cdccfe1da6d50f61f13
Henry Taylor <hjt8@duke.edu>
2017-05-02 16:41:20 -0400

    comit to pull

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,12 @@
-import gameView.loginScreen.LoginScreen;
 import controller.Controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import voogasalad.util.paint.PaintWindow;
 
 public class Main extends Application {
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-//		LoginScreen l = new LoginScreen(null);
-//		arg0.setScene(l.getScene());
-//		arg0.show();
 		new Controller(arg0);
-		//TODO: Uncomment the line below and comment the line above to enter test mode for worldanimation and gameengine
-		//new RunnerTest(arg0);  
-		  
-		//new DrawingCanvas(arg0);  
-		//new PaintWindow();
 	}
 
 	public static void main(String[] args) {
--- src/data_interfaces/Communicator.java
+++ src/data_interfaces/Communicator.java
@@ -68,79 +68,79 @@ import gamedata.GameData;
 import javafx.scene.input.KeyCode;
 
 public class Communicator extends GameSavingDataTool implements EngineCommunication, PlayerCommunication {
-
+//
+//	
+//	private String fileName;
+//	List<Map> results;
+//
+//	public Communicator(String s) {
+//		System.out.println("COMMUNICATOR MADE: " + s);
+//		fileName = s;
+//		XMLPlacedParser xp = new XMLPlacedParser();
+//		results = xp.getData(fileName);
+//	}
 	
-	private String fileName;
-	List<Map> results;
-
-	public Communicator(String s) {
-		System.out.println("COMMUNICATOR MADE: " + s);
-		fileName = s;
-		XMLPlacedParser xp = new XMLPlacedParser();
-		results = xp.getData(fileName);
-	}
-	
-	public List<IEntityManager> getIEntityManagers() {
-		Map <Integer, Map<Integer, Entity>> m = results.get(0);
-		List <IEntityManager> ret = new ArrayList<IEntityManager>();
-		List <IEntity> toBeAdded;
-		
-		System.out.println("m size: " + m.size());
-		
-		for (int i = 1; i <= m.size(); i++) {
-			System.out.println("THISSSSS happens");
-			toBeAdded = new ArrayList<IEntity>();
-			for (Integer j: m.get(i).keySet()) {
-				toBeAdded.add(m.get(i).get(j));
-				System.out.println("this happens");
-			}
-			ret.add(new EntityManager(toBeAdded));
-			
-		}
-		return ret;
-		
-	}
-
-
-
-	@Override
-	public List<LevelEntity> getLevelEntities() {
-		Map <Integer, LevelEntity> m = results.get(1);
-		List<LevelEntity> ret = new ArrayList<LevelEntity>();
-		
-		for (int i = 1; i <= m.size(); i++) {
-			ret.add(m.get(i));
-		}
-		System.out.println("HOW MANY LEVEL ENTITIES" + ret.size());
-		
-		return ret;
-	}
-
-
-
-	@Override
-	public SplashData getSplashEntity() {
-		return (SplashData) results.get(2).get(getSplashConstant());
-	}
-
-
-
-	@Override
-	public InfiniteEnum getInfinite() {
-		Map<Integer, LevelEntity> m = results.get(1);
-		System.out.println(m.get(1).getInfiniteEnum());
-		return m.get(1).getInfiniteEnum();
-	}
-	
-	public String getMusic() {
-		Map<Integer, LevelEntity> m = results.get(1);
-		return m.get(1).getMusic();
-	}
-	
-	public int getLives () {
-		Map <Integer, LevelEntity> m = results.get(1);
-		return m.get(1).getLives();
-	}
+//	public List<IEntityManager> getIEntityManagers() {
+//		Map <Integer, Map<Integer, Entity>> m = results.get(0);
+//		List <IEntityManager> ret = new ArrayList<IEntityManager>();
+//		List <IEntity> toBeAdded;
+//		
+//		System.out.println("m size: " + m.size());
+//		
+//		for (int i = 1; i <= m.size(); i++) {
+//			System.out.println("THISSSSS happens");
+//			toBeAdded = new ArrayList<IEntity>();
+//			for (Integer j: m.get(i).keySet()) {
+//				toBeAdded.add(m.get(i).get(j));
+//				System.out.println("this happens");
+//			}
+//			ret.add(new EntityManager(toBeAdded));
+//			
+//		}
+//		return ret;
+//		
+//	}
+//
+//
+//
+//	@Override
+//	public List<LevelEntity> getLevelEntities() {
+//		Map <Integer, LevelEntity> m = results.get(1);
+//		List<LevelEntity> ret = new ArrayList<LevelEntity>();
+//		
+//		for (int i = 1; i <= m.size(); i++) {
+//			ret.add(m.get(i));
+//		}
+//		System.out.println("HOW MANY LEVEL ENTITIES" + ret.size());
+//		
+//		return ret;
+//	}
+//
+//
+//
+//	@Override
+//	public SplashData getSplashEntity() {
+//		return (SplashData) results.get(2).get(getSplashConstant());
+//	}
+//
+//
+//
+//	@Override
+//	public InfiniteEnum getInfinite() {
+//		Map<Integer, LevelEntity> m = results.get(1);
+//		System.out.println(m.get(1).getInfiniteEnum());
+//		return m.get(1).getInfiniteEnum();
+//	}
+//	
+//	public String getMusic() {
+//		Map<Integer, LevelEntity> m = results.get(1);
+//		return m.get(1).getMusic();
+//	}
+//	
+//	public int getLives () {
+//		Map <Integer, LevelEntity> m = results.get(1);
+//		return m.get(1).getLives();
+//	}
 	
 	
 	
@@ -957,17 +957,17 @@ public class Communicator extends GameSavingDataTool implements EngineCommunicat
 ////		//
 ////		//
 ////		//		((KeyInputComponent) x.getComponent(ComponentType.KeyInput)).addToMap(KeyCode.T, "REMOVE");
-//		Collection<IEntity> e8 = new ArrayList<IEntity>();
-//		for (Entity exp : e7) {
-//			e8.add(exp);
-//		}
-//		IEntityManager myEntityManager = new EntityManager(e1);
-//		List<IEntityManager> myEntityManagers= new ArrayList<IEntityManager>();
-//		myEntityManagers.add(myEntityManager);
-//		myEntityManagers.add(new EntityManager(e8));
-//		return myEntityManagers;
-////		return myGameData;
-//	}
+		Collection<IEntity> e8 = new ArrayList<IEntity>();
+		for (Entity exp : e7) {
+			e8.add(exp);
+		}
+		IEntityManager myEntityManager = new EntityManager(e1);
+		List<IEntityManager> myEntityManagers= new ArrayList<IEntityManager>();
+		myEntityManagers.add(myEntityManager);
+		myEntityManagers.add(new EntityManager(e8));
+		return myEntityManagers;
+//		return myGameData;
+	}
 //	private Entity createPortal() {
 //		Entity portal2 = new Entity(110);
 //		portal2.addComponent(new LocationComponent(450, 125));
