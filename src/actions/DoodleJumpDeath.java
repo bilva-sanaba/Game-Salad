b6b95c6f5da726ad50d74c070f343d7f42637e51
Hamsa <hsp8@duke.edu>
2017-05-04 01:43:30 -0400

    doodlejump

---
+++ src/actions/DoodleJumpDeath.java
@@ -0,0 +1,30 @@
+package actions;
+
+import components.entityComponents.ComponentType;
+import components.entityComponents.ImagePropertiesComponent;
+import components.entityComponents.LocationComponent;
+import components.entityComponents.VelocityComponent;
+import entity.IEntity;
+import entity.IEntityManager;
+import gamedata.IRestrictedGameData;
+
+public class DoodleJumpDeath extends AbstractAction implements IAction{
+
+	@Override
+	public IRestrictedGameData executeAction(IEntity other, IEntity self, IEntityManager myEM,
+			IRestrictedGameData currentGameData) {
+		VelocityComponent vc = (VelocityComponent) other.getComponent(ComponentType.Velocity);
+		IRestrictedGameData ret = currentGameData;
+		if (vc != null) {
+			double yVelo = vc.getY();
+			if (yVelo > 0) {
+				ret = new DeathAction().executeAction(other, self, myEM, currentGameData);
+			}
+			
+		}
+		return getGameDataFactory().blankEntityData(ret);
+	}
+
+	
+
+}
